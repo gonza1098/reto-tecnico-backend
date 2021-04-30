89828771060c1a9401f347b9a45623761c68677b
gonza1098 <gramirezm80@gmail.com>
2021-04-30 10:07:23 -0500

    first commit

---
+++ src/people.js
@@ -0,0 +1,166 @@
+'use strict';
+const AWS = require('aws-sdk'); 
+const fetch = require('node-fetch');
+
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'}); //conexion dynamodb 
+
+const returnResponse = (statusCode, params, callback) => {
+  const response = {
+    statusCode: statusCode,
+    headers:{
+      'Access-Control-Allow-Origin':'*', //requerido para CORS
+    },
+    body:  JSON.stringify(params)
+  };
+
+  callback(null, response)
+};
+
+module.exports.getPeople = async (event, context, callback) => {
+  try {
+    let  nextQuery = event["queryStringParameters"];
+
+    const  dynomoParams = {
+      TableName: 'People',
+      // Limit: 3,
+      // ExclusiveStartKey: {
+      //   "people_id": "83f58c25-8f7f-4522-8740-ab8ac0d62c42"
+      // }
+    };
+
+    let peoples = await ddb.scan(dynomoParams).promise();
+
+    let params = {
+      // "next": "", 
+      // "previous": "", 
+      "mensaje": 'Personas listadas correctamente',
+      // nextPage: nextQuery,
+      "results": peoples.Items,
+      "estado":true
+    }
+
+    returnResponse(200, params, callback);
+
+  } catch (err) {
+
+    let params = {
+      "mensaje": err.message||'Error en listar personas',
+      "estado": false
+    }
+
+    returnResponse(404, params, callback);
+
+  }
+};
+
+module.exports.getPeopleById = async (event, context, callback) => {
+  try {
+    let  idPeople =  event['pathParameters'].id;
+    const  dynomoParams = {
+      TableName: 'People',
+      Key: {
+        "persona_id": idPeople        
+      },
+      // ProjectionExpression: 'ATTRIBUTE_NAME'
+    };
+
+    let people = await ddb.get(dynomoParams).promise();
+
+    if (!people.Item) {
+      const url = `https://swapi.py4e.com/api/people/${idPeople}`;
+      const swapiParams = {
+        method: "GET",
+      };
+      let params = {};
+
+      await fetch(url, swapiParams).then(res => res.json())
+      .then(json => {
+        params = {
+          "mensaje": json.detail == 'Not found' ? 'Id no encontrado o no registrado':'Persona listada correctamente de SWAPI',
+          "people":  json.detail == 'Not found' ? null:json,
+          "estado": json.detail == 'Not found' ? false:true
+        }
+        returnResponse(200, params, callback);
+      })
+      .catch(err => {
+        console.log(err);
+        params = {
+          "mensaje": err.message||'Error en listar personas',
+          "estado": false
+        }
+        returnResponse(404, params, callback);
+      });
+      // returnResponse(404, params, callback);
+    }else{
+      let params = {
+        "mensaje": 'Persona listada correctamente de DynamoDB',
+        "people":   people.Item,
+        "estado": true
+      }
+      returnResponse(200, params, callback);
+    }
+
+  } catch (err) {
+
+    let params = {
+      "mensaje": err.message||'Error en listar personas',
+      "estado": false
+    }
+
+    returnResponse(404, params, callback);
+
+  }
+};
+
+module.exports.createPeople = async (event, context, callback) => {
+  try {
+
+    const host = event.headers.Host;
+    const path = event.requestContext.path;
+    let body = JSON.parse(event.body);
+    const requestId = context.awsRequestId;
+
+    const  dynomoParams = {
+      TableName: 'People',
+      Item: {
+        'persona_id' : requestId,
+        "nombre": body.nombre, 
+        "altura": body.altura, 
+        "masa": body.masa, 
+        "color_de_pelo": body.color_de_pelo, 
+        "color_de_piel": body.color_de_piel, 
+        "color_de_ojos": body.color_de_ojos, 
+        "fecha_de_nacimiento": body.fecha_de_nacimiento, 
+        "genero": body.genero, 
+        "mundo_natal": body.mundo_natal, 
+        "peliculas": body.peliculas, 
+        "especies": body.especies, 
+        "vehiculos": body.vehiculos, 
+        "naves_estelares": body.naves_estelares,   
+        "creado": new Date(), 
+        "editado": new Date(), 
+        "url": `https://${host}${path}/${requestId}`
+      },
+      ReturnValues: 'ALL_OLD'
+    };
+
+    const newPeople = await ddb.put(dynomoParams).promise();
+
+    let params = {
+      "mensaje": 'Persona creada  satisfactoriamente',
+      // people: dynomoParams.Item,
+      "estado":true
+    }
+
+    returnResponse(200, params, callback);
+
+  } catch (err) {
+
+    let params = {
+      "mensaje": err.message||'Error en crear persona',
+      "estado": false
+    }
+
+    returnResponse(404, params, callback);
+  }
+};
